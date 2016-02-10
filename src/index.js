4f371ab00f03cb691a96a328f1cadcef30ab48a1
Simon Bruce <simon@blacksun.cx>
2016-02-10 22:19:12 +0000

    Fixed up some bugs.

--- src/index.js
+++ src/index.js
@@ -5,24 +5,29 @@ const fs = require("fs");
 const tsm = require("teamcity-service-messages");
 
 function outputPackageParameters() {
-  const packageJson = require("./package.json");
+  fs.readFile("./package.json", (err, data) => {
+    if (err) {
+      console.error("Could not read the package.json", err);
+    }
 
-  if (packageJson.name) {
-    tsm.setParameter({"PACKAGE_NAME": packageJson.name});
-  }
+    const packageJson = JSON.parse(data);
 
-  if (packageJson.description) {
-    tsm.setParameter({"PACKAGE_DESCRIPTION": packageJson.description});
-  }
+    if (packageJson.name) {
+      tsm.setParameter({"PACKAGE_NAME": packageJson.name});
+    }
 
-  if (packageJson.version) {
-    tsm.setParameter({"PACKAGE_VERSION": packageJson.version});
-  }
+    if (packageJson.description) {
+      tsm.setParameter({"PACKAGE_DESCRIPTION": packageJson.description});
+    }
 
-  if (packageJson.license) {
-    tsm.setParameter({"PACKAGE_LICENSE": packageJson.license});
-  }
+    if (packageJson.version) {
+      tsm.setParameter({"PACKAGE_VERSION": packageJson.version});
+    }
 
+    if (packageJson.license) {
+      tsm.setParameter({"PACKAGE_LICENSE": packageJson.license});
+    }
+  });
 }
 
 fs.access("./package.json", fs.F_OK | fs.R_OK, (err) => {
@@ -30,7 +35,6 @@ fs.access("./package.json", fs.F_OK | fs.R_OK, (err) => {
     tsm.buildProblem({"description": "No package.json file found in working directory."});
     process.exit(1);
   } else {
-    console.log("test");
     outputPackageParameters();
   }
 });
